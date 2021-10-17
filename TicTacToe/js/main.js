7afdcddfda0742e254c57a5ecd56cc22a419d598
shabbnam <shabrnd@gmail.com>
2021-10-17 18:25:28 +0530

    tictac toe

---
+++ TicTacToe/js/main.js
@@ -0,0 +1,20 @@
+import Game from "./Game.js"
+import GameView from "./GameView.js"
+let game = new Game()
+let gameView = new GameView(document.getElementById("app"))
+
+gameView.onTileClick = function (index) {
+    game.makeMove(index)
+    gameView.update(game)
+
+}
+
+gameView.onRestartClick  = function () {
+    game = new Game()
+    gameView.update(game)
+}
+
+gameView.update(game)
+
+
+
